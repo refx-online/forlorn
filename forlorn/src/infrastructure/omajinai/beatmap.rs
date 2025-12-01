d017c9c7839003adbf6192b5689d90bc6ab4b177
remeliah <anoletenu@gmail.com>
2025-12-01 16:35:52 +0700

    revert: it was permission issue.

--- forlorn/src/infrastructure/omajinai/beatmap.rs
+++ forlorn/src/infrastructure/omajinai/beatmap.rs
@@ -1,7 +1,6 @@
-use std::{sync::LazyLock, time::Duration};
+use std::sync::LazyLock;
 
 use anyhow::Result;
-use tokio::time;
 
 use crate::config::OmajinaiConfig;
 
@@ -10,20 +9,8 @@ static CLIENT: LazyLock<reqwest::Client> = LazyLock::new(reqwest::Client::new);
 pub async fn fetch_beatmap(config: &OmajinaiConfig, beatmap_id: i32) -> Result<Vec<u8>> {
     let url = format!("{}/get-osu/{beatmap_id}", config.beatmap_service_url);
 
-    for att in 0..3 {
-        let resp = CLIENT.get(&url).send().await?;
+    let resp = CLIENT.get(&url).send().await?;
+    let bytes = resp.bytes().await?;
 
-        let bytes = resp.bytes().await?;
-
-        if bytes.starts_with(b"osu file format") {
-            return Ok(bytes.to_vec());
-        }
-
-        // beatmap service/my wifi is too slow!, retry.
-        if att < 2 {
-            time::sleep(Duration::from_millis(100 * (1 << att))).await;
-        }
-    }
-
-    unreachable!()
+    Ok(bytes.to_vec())
 }
